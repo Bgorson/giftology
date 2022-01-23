cf70d88b4dd00e5431da0dd7bd2cb2b11dc016fb
bgorson <bgorson@foxtrotco.com>
2022-01-23 10:55:39 -0600

    added target blank links

--- client/components/atoms/Card/Card.js
+++ client/components/atoms/Card/Card.js
@@ -11,26 +11,28 @@ export default function ProductCard({ product }) {
   let parsedImage =
     product.htmlTag.split('src')[1]?.substring(2)?.slice(0, -12) || '';
   console.log(product);
-  console.log(product);
+  console.log(product.link);
 
   return (
     <Card style={{ height: '390px' }}>
-      <CardMedia
-        component="img"
-        alt={product.productName}
-        height="200"
-        image={
-          product.website === 'Etsy' ? product.directImageSrc : parsedImage
-        }
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {product.productName}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {product.flavorText}
-        </Typography>
-      </CardContent>
+      <CardActionArea href={product.link} target="_blank">
+        <CardMedia
+          component="img"
+          alt={product.productName}
+          height="200"
+          image={
+            product.website === 'Etsy' ? product.directImageSrc : parsedImage
+          }
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {product.productName}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {product.flavorText}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
