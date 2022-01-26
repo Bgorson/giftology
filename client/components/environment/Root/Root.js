a3d0d6596240da75cd90d165bfc87d7edc5d09b2
bgorson <bgorson@foxtrotco.com>
2022-01-25 18:21:29 -0600

    actually set it up

--- client/components/environment/Root/Root.js
+++ client/components/environment/Root/Root.js
@@ -4,7 +4,7 @@ import ReactGA from 'react-ga';
 
 import { BrowserRouter } from 'react-router-dom';
 import Main from '../Main';
-ReactGA.initialize('300818648');
+ReactGA.initialize('UA-218196758-1');
 console.log('visit');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
