c0416d1ae8746900fe5248bbe6d6a4ab1fc5a04c
bgorson <bgorson@foxtrotco.com>
2022-01-25 18:08:18 -0600

    adding google analytics

--- client/components/environment/Root/Root.js
+++ client/components/environment/Root/Root.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+
 import { BrowserRouter } from 'react-router-dom';
 import Main from '../Main';
+ReactGA.initialize('300818648');
+console.log('visit');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function Root() {
   return (
