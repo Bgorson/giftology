8952c0f57dd3e2575dc6dd83b3fbbbc9c2adc90a
bgorson <bgorson@foxtrotco.com>
2022-04-09 18:28:43 -0500

    update db seeding

--- server/database/index.js
+++ server/database/index.js
@@ -17,19 +17,22 @@ const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-console.log(process.env.DATABASE_URL);
 const database = mongoose
-  .connect(process.env.DATABASE_URL, options)
-  .then(() => console.log('Connected to database.'))
-  .then(() =>
+  .connect(
+    process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/Giftology',
+    options
+  )
+  .then((db) =>
     (async function () {
+      console.log('Connected to database.');
+
       try {
-        const conn = await mongoose.connect(
-          'mongodb://127.0.0.1:27017/Giftology'
-        );
+        // const conn = await mongoose.connect(
+        //   process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/Giftology'
+        // );
 
         await Promise.all(
-          Object.entries(conn.models).map(([k, m]) => m.deleteMany())
+          Object.entries(db.models).map(([k, m]) => m.deleteMany())
         );
 
         let headers = Object.keys(Product.schema.paths).filter(
