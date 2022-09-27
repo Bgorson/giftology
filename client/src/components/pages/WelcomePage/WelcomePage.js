91c23740e6937eba2788b395097036f997758e75
bgorson <bgorson@foxtrotco.com>
2022-09-26 20:03:27 -0500

    attempting to improve seo

--- client/src/components/pages/WelcomePage/WelcomePage.js
+++ client/src/components/pages/WelcomePage/WelcomePage.js
@@ -16,13 +16,13 @@ export default function WelcomePage() {
   return (
     <div className="welcome-page page">
       <Hero>
-        <HeroImage src={backgroundImage} />
+        <HeroImage alt={'Gift Quiz'} src={backgroundImage} />
         <HeroText>
           <HeroTitle>Having Trouble Finding The Right Gift?</HeroTitle>
           <HeroDescription>
-            Check out our quiz and we’ll do the searching for you. All you need
-            to know is who you’re shopping for and what they do for fun. We’ll
-            handle the rest.
+            Check out our gift quiz and we’ll do the searching for you. All you
+            need to know is who you’re shopping for and what they do for fun.
+            We’ll handle the rest.
           </HeroDescription>
           <HeroCallToAction as={Link} to="/quiz">
             Take The Quiz
