3d4725fe020b96bd582cd768290ff1439949c0a3
bgorson <bgorson@foxtrotco.com>
2022-10-05 20:22:17 -0500

    update route

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(session(sess));
 app.use(bodyParser.json());
 
 app.use('/api', routes);
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
