03cc4193f577e9005ef7a1a227a4e8a7641a0963
Brandon Gorson <Brandon.Gorson@dentsu.com>
2023-06-13 11:05:38 -0500

    Updated Demo page

---
+++ client/src/components/organisms/ProductSwipeContainer/ProductSwipeContainer.js
@@ -0,0 +1,151 @@
+import TinderCard from "react-tinder-card";
+import React, { useState, useRef, useMemo } from "react";
+import { Audio } from "react-loader-spinner";
+import styled, { keyframes } from "styled-components";
+
+const CardContainer = styled.div`
+  width: 90vw;
+  max-width: 260px;
+  height: 300px;
+`;
+const Card = styled.div`
+  position: relative;
+  background-color: #fff;
+  color: salmon;
+  font-size: 20px;
+  width: 80vw;
+  max-width: 260px;
+  height: 300px;
+
+  background-size: cover;
+  background-position: center;
+  background-image: url(${(props) => props.url});
+`;
+
+const StyledTinderCard = styled(TinderCard)`
+  position: absolute;
+`;
+const popUpAnimation = keyframes`
+  0%   { transform: scale(1,1) }
+  10%  { transform: scale(1.1,1.1) }
+  30%  { transform: scale(.9,.9) }
+  50%  { transform: scale(1,1) }
+  57%  { transform: scale(1,1) }
+  64%  { transform: scale(1,1) }
+  100% { transform: scale(1,1) }
+}
+`;
+const InfoText = styled.h2`
+  width: 100%;
+  justify-content: center;
+  display: flex;
+  color: red;
+  animation-name: popUpAnimation;
+  animation-duration: 800ms;
+`;
+
+function ProductSwipeContainer({ data }) {
+  const [currentIndex, setCurrentIndex] = useState(data.length - 1);
+  const [lastDirection, setLastDirection] = useState();
+
+  // used for outOfFrame closure
+  const currentIndexRef = useRef(currentIndex);
+
+  const childRefs = useMemo(
+    () =>
+      Array(data.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+    []
+  );
+
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  };
+
+  const canGoBack = currentIndex < data.length - 1;
+
+  const canSwipe = currentIndex >= 0;
+
+  // set last direction and decrease current index
+  const swiped = (direction, nameToDelete, index) => {
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
+  };
+
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+    // TODO: when quickly swipe and restore multiple times the same card,
+    // it happens multiple outOfFrame events are queued and the card disappear
+    // during latest swipes. Only the last outOfFrame event should be considered valid
+  };
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < data.length) {
+      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
+    }
+  };
+
+  // increase current index and show card
+  const goBack = async () => {
+    if (!canGoBack) return;
+    const newIndex = currentIndex + 1;
+    updateCurrentIndex(newIndex);
+    await childRefs[newIndex].current.restoreCard();
+  };
+
+  return (
+    <div>
+      <>
+        <h1>React Tinder Card</h1>
+        <CardContainer>
+          {data.map((character, index) => (
+            <StyledTinderCard
+              ref={childRefs[index]}
+              className="swipe"
+              key={character.productName}
+              onSwipe={(dir) => swiped(dir, character.productName, index)}
+              onCardLeftScreen={() => outOfFrame(character.productName, index)}
+            >
+              <Card url={character.directImageSrc}>
+                <h3>{character.productName}</h3>
+              </Card>
+            </StyledTinderCard>
+          ))}
+        </CardContainer>
+
+        {lastDirection ? (
+          <InfoText>You swiped {lastDirection}</InfoText>
+        ) : (
+          <InfoText>
+            Swipe a card or press a button to get Restore Card button visible!
+          </InfoText>
+        )}
+        <div className="buttons">
+          <button
+            style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+            onClick={() => swipe("left")}
+          >
+            Swipe left!
+          </button>
+          <button
+            style={{ backgroundColor: !canGoBack && "#c3c4d3" }}
+            onClick={() => goBack()}
+          >
+            Undo swipe!
+          </button>
+          <button
+            style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+            onClick={() => swipe("right")}
+          >
+            Swipe right!
+          </button>
+        </div>
+      </>
+    </div>
+  );
+}
+export default ProductSwipeContainer;
--- client/src/components/pages/DemoPage/DemoPage.js
+++ client/src/components/pages/DemoPage/DemoPage.js
@@ -2,8 +2,31 @@ import React, { useEffect, useContext, useState } from "react";
 import { postGPT } from "../../../api/gpt";
 import { hobbyMap } from "../../../utils/hobbyMap";
 import { Audio } from "react-loader-spinner";
+import { postAllQuizResults } from "../../../api/allQuiz";
+import ProductSwipeContainer from "../../organisms/ProductSwipeContainer/ProductSwipeContainer";
+import { useQuery } from "@tanstack/react-query";
 
 export default function DemoPage() {
+  const quizPayload = {
+    who: "myself",
+    name: "someone",
+    age: "21-44",
+    occasion: "holiday",
+    hobbies: ["healthAndWellness", "outdoorGames"],
+    type: ["thoughtful"],
+    tags: ["efficient", "handy", "nerdy", "tea", "homeDecor", "dogs"],
+  };
+
+  const fetchExampleData = (quizPayload) => async () => {
+    const response = await postAllQuizResults(quizPayload, "");
+    return response.products;
+  };
+  const {
+    data,
+    isLoading: queryLoading,
+    error,
+  } = useQuery(["exampleQuery"], fetchExampleData(quizPayload));
+
   const [prompt, setPrompt] = useState({
     age: "",
     occassion: "",
@@ -57,80 +80,89 @@ export default function DemoPage() {
     setPrompt({ ...prompt, tags: selectedOptions });
   };
   return (
-    <div
-      style={{
-        width: "75%",
-        gap: "1em",
-        margin: "auto",
-        paddingTop: "5em",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <h1>OpenAI GPT-3 Demo</h1>
-      <label>AGE- needs to be entered as age-age</label>
-      <p>For Example: 18-21</p>
-      <input
-        onChange={(e) => {
-          setPrompt({ ...prompt, age: e.target.value });
+    <>
+      <div
+        style={{
+          width: "50%",
+          gap: "1em",
+          margin: "auto",
+          paddingTop: "5em",
+          // display: "flex",
+          // flexDirection: "column",
         }}
-      />
-      <label>OCCASSION</label>
-      <input
-        onChange={(e) => {
-          setPrompt({ ...prompt, occassion: e.target.value });
-        }}
-      />
-      <label>HOBBIES</label>
-      <select multiple onChange={handleHobbiesChange}>
-        {hobbyMap.map((hobby) => (
-          <option key={hobby.value} value={hobby.message}>
-            {hobby.message}
-          </option>
-        ))}
-      </select>
-      <label>Type</label>
-      <select multiple onChange={handleTypeChange}>
-        {typeMap.map((type) => (
-          <option key={type.value} value={type.message}>
-            {type.message}
-          </option>
-        ))}
-      </select>
-      <label>Tags</label>
-      <select multiple onChange={handleTagChange}>
-        {tagsMap.map((tag) => (
-          <option key={tag.value} value={tag.message}>
-            {tag.message}
-          </option>
-        ))}
-      </select>
-
-      <h2>Response</h2>
-      {response && (
-        <p style={{ whiteSpace: "pre-line" }}>{response.message.content}</p>
-      )}
-      {isLoading ? (
-        <Audio />
-      ) : (
-        <button
-          onClick={(e) => {
-            setIsLoading(true);
-            setResponse("");
-            e.preventDefault();
-            postGPT(prompt).then((res) => {
-              setIsLoading(false);
-              if (res) {
-                setResponse(res);
-              } else {
-                setResponse({ message: { content: "No response from API" } });
-              }
-            });
+      >
+        <h1>OpenAI GPT-3 Demo</h1>
+        <label>AGE- needs to be entered as age-age</label>
+        <p>For Example: 18-21</p>
+        <input
+          onChange={(e) => {
+            setPrompt({ ...prompt, age: e.target.value });
           }}
-        >
-          SUBMIT
-        </button>
-      )}
-    </div>
+        />
+        <label>OCCASSION</label>
+        <input
+          onChange={(e) => {
+            setPrompt({ ...prompt, occassion: e.target.value });
+          }}
+        />
+        <label>HOBBIES</label>
+        <select multiple onChange={handleHobbiesChange}>
+          {hobbyMap.map((hobby) => (
+            <option key={hobby.value} value={hobby.message}>
+              {hobby.message}
+            </option>
+          ))}
+        </select>
+        <label>Type</label>
+        <select multiple onChange={handleTypeChange}>
+          {typeMap.map((type) => (
+            <option key={type.value} value={type.message}>
+              {type.message}
+            </option>
+          ))}
+        </select>
+        <label>Tags</label>
+        <select multiple onChange={handleTagChange}>
+          {tagsMap.map((tag) => (
+            <option key={tag.value} value={tag.message}>
+              {tag.message}
+            </option>
+          ))}
+        </select>
+
+        <h2>Response</h2>
+        {response && (
+          <p style={{ whiteSpace: "pre-line" }}>{response.message.content}</p>
+        )}
+        {isLoading ? (
+          <Audio />
+        ) : (
+          <button
+            onClick={(e) => {
+              setIsLoading(true);
+              setResponse("");
+              e.preventDefault();
+              postGPT(prompt).then((res) => {
+                setIsLoading(false);
+                if (res) {
+                  setResponse(res);
+                } else {
+                  setResponse({ message: { content: "No response from API" } });
+                }
+              });
+            }}
+          >
+            SUBMIT
+          </button>
+        )}
+      </div>
+      <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+        {queryLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <ProductSwipeContainer data={data} />
+        )}
+      </div>
+    </>
   );
 }
