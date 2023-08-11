381a26f6a98ac4368f4fcb3184a0152af27b5d56
BGorson <BGorson32@gmail.com>
2023-08-10 22:02:30 -0500

    fix server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const session = require('express-session');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const session = require("express-session");
 
-require('./database');
+require("./database");
 
-require('./config/environment');
+require("./config/environment");
 
-const routes = require('./routes/index');
+const routes = require("./routes/index");
 
 const port = process.env.PORT;
 const app = express();
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')), { maxAge: '30d' });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -25,9 +25,9 @@ app.use(session(sess));
 
 app.use(bodyParser.json());
 
-app.use('/api', routes);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.use("/api", routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
